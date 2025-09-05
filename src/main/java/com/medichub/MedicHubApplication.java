0117a98a2bea014a5bc87784853d208cd797e2b3
kashkina.svitlana <gssrzr@zetu>
2025-09-05 12:04:26 +0200

    Initial commit - MedicHub Projektstruktur

---
+++ src/main/java/com/medichub/MedicHubApplication.java
@@ -0,0 +1,14 @@
+package com.medichub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicHubApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MedicHubApplication.class, args);
+	}
+
+}
