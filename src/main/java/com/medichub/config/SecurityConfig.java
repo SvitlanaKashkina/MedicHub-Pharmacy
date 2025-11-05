830fb93a67a89903494db42391cda59c4621cb43
kashkina.svitlana <gssrzr@zetu>
2025-11-05 16:52:51 +0100

    error correction

--- src/main/java/com/medichub/config/SecurityConfig.java
+++ src/main/java/com/medichub/config/SecurityConfig.java
@@ -5,11 +5,8 @@ import com.medichub.service.cart.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -54,17 +51,5 @@ public class SecurityConfig {
                 );
         return http.build();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
-    }
-
 }
 
